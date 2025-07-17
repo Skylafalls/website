ae4bf8a8cb6fc87e36617bbee23f5bf13b48c720
Thien-Hubbing <triplettran56@gmail.com>
2025-07-17 12:42:44 -0400

    Hopefully fixed that annoying thing

---
+++ app/pages/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello, World!</h1>
+    <p>This is
+      <a href="https://youtube.com/@boredthien" aria-label="Link to YT Channel">
+        Thien's
+      </a>
+      website, built with zero
+      <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" aria-label="Link to MDN's CSS">
+        CSS
+      </a>
+      (as I found it way too annoying) and for some reason 
+      <a href="https://nuxt.com" aria-label="Link to Nuxt Framework">
+        Nuxt.js.
+      </a>
+      <br>
+      The source code for the website can be found
+      <a href="https://github.com/Thien-Hubbing/Thien-Hubbing.github.io">
+        here.
+      </a>
+    </p>
+  </div>
+</template>
